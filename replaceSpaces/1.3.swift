ec068ba88e6a6407751f4102100c7d00d0751fc4
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-05-17 18:40:43 +0300

    replaceSpaces

---
+++ replaceSpaces/1.3.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+/* Напишите метод, заменяющий все пробелы в строке символами '%20'. Можете считать, что длина строки позволяет сохранить дополнительные символы, а фактическая длина строки известна заранее.
+ */
+
+func replaceSpaces(in string: String, upTo length: Int) -> String {
+    let newStringLength: Int
+    if length > string.count {
+        newStringLength = string.count
+    } else {
+        newStringLength = length
+    }
+    var i = string.startIndex
+    var newString = ""
+    var j = string.startIndex
+    for _ in 0..<newStringLength {
+        if string[i] == " " {
+            newString.append("%20")
+            j = newString.endIndex
+        } else {
+            newString.insert(string[i], at: j)
+            j = newString.index(after: j)
+        }
+        i = string.index(after: i)
+    }
+    return newString
+}
+
+
+print(replaceSpaces(in: "Mr John Smith     ", upTo: 13))
